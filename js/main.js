e6691883a6c5476d24984edf766b8183665df574
Michele <micheleierniero@gmail.com>
2021-09-08 14:47:59 +0200

    js slider photos

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 Vue.config.devtools = true;
 
-var app = new Vue (
-    {
-        el: "#root"
+const app = new Vue ({
+    el: ".container",
+    data: {
+        counterPhoto: 0,
+        photos: [
+            "img/image1.jpg",
+            "img/image2.jpg",
+            "img/image3.jpg",
+            "img/image4.jpg",
+        ]
+    },
+    methods: {
+        prevPhoto() {
+            this.counterPhoto -= 1;
+            if (this.counterPhoto < 0) this.counterPhoto = (this.photos.length - 1);
+        },
+        nextPhoto() {
+            this.counterPhoto += 1;
+            if (this.counterPhoto == (this.photos.length)) this.counterPhoto = 0;
+        }
     }
-)
+});
